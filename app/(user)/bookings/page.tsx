4ffc6d54b6e6bb0b5f5f0e3b22b42e094641b640
honorine22 <higiranez@gmail.com>
2025-02-20 18:01:08 +0200

    Booking dashboard for renters

---
+++ app/(user)/bookings/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RenterDashboard from '../../components/RenterDashboard'
+
+function page() {
+    return (
+        <RenterDashboard />
+    )
+}
+
+export default page
